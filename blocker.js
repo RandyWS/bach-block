8207cda50b5e6b679b9639cee6db9c825024eff9
Miranda Stopa <miranda.stopa@gmail.com>
2021-11-12 14:43:01 -0500

    finished project

--- blocker.js
+++ blocker.js
@@ -1,39 +1,13 @@
-const keywords = ["bachelor", "bachelorette"];
-
-// Listeners to listen when the page loads
-var t = setTimeout(function () {
-  $(function () {
-    searchForSpoilers(keywords);
-  });
-}, 1000);
+const item = "bachelor";
 
 $(function () {
-  searchForSpoilers(keywords);
-
-  // New up an observer, and tell it what to do when it successfully observes.
-  // Necessary for Facebooks "neverending" scrolling
-  var observer = new MutationObserver(function (mutations, observer) {
-    // fired when a mutation occurs
-    searchForSpoilers(keywords);
-  });
-
-  // This part establishes what needs to be watched, and starts the watching
-  observer.observe($("[id^=topnews_main_stream_]").get(0), {
-    subtree: true, // watches target and it's descendants
-    attributes: true, // watches targets attributes
-  });
+  searchForSpoilers();
 });
 
-// Handles searching for spoilers
 function searchForSpoilers() {
-  if (keywords.length) {
-    var search = "";
-    keywords.forEach(function (item) {
-      search = search + "p:contains('" + item + "'), ";
-    });
-    search = search.substring(0, search.length - 2);
+  let search = "";
 
-    $(search).parent(".userContentWrapper").css("filter", "blur(5px)");
-    $(search).parent().css("filter", "blur(5px)");
-  }
+  search = search + "div:contains('" + item + "'), ";
+  search = search.substring(0, search.length - 2);
+  $(search).css("filter", "blur(3px)");
 }
